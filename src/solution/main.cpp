27d0be80fcb8882825483dbca059cae5d4864514
Alexander Heinlein <alexander.heinlein@uni-koeln.de>
2021-11-25 12:08:48 +0100

    Small correction of the parameter lists

--- src/solution/main.cpp
+++ src/solution/main.cpp
@@ -126,7 +126,7 @@ int main (int argc, char *argv[])
     ////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
     if (verbose) cout << endl;
-    if (verbose) cout << ">> III. Construct Schwarz Preconditioner\n";
+    if (verbose) cout << ">> III. Construct Schwarz preconditioner\n";
     if (verbose) cout << endl;
 
     // FROSch preconditioner for Belos
