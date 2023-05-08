88fc7f87cafca63e74df8361c52f0013d0ab18c6
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-05-08 22:35:25 +0200

    add preliminary job scripts for lab3 and adapt to use likwid markers

---
+++ exercises/lab3/run_on_cpu_with_likwid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --exclusive
+#SBATCH --time=00:10:00
+#SBATCH --mem-per-cpu=3GB
+
+#SBATCH --account=education-eemcs-courses-wi4450
+#SBATCH --reservation=wi4450
+#SBATCH --partition=compute
+
+
+source trilinos-env.sh
+cd build
+srun likwid-perfctr -m -C 0-47 -g MEM_DP ./ifpack_driver.x --matrixFilename="../data/lap128x128.mtx" --rhsFilename="../data/rhs128x128.mtx"
--- exercises/lab3/trilinos-env.sh
+++ exercises/lab3/trilinos-env.sh
@@ -4,8 +4,12 @@ module use /apps/generic/compiler/lmod/linux-rhel8-x86_64/Core
 module load gcc/11.3.0
 module use /apps/arch/2023rc1/lmod/linux-rhel8-x86_64/gcc/11.3.0
 
-module load openmpi cuda/11.6 cmake openblas
+module use /beegfs/apps/unsupported/slurm-overload
 
-export TRILINOS_ROOT=/beegfs/apps/unsupported/trilinos-devel/
-export OMPI_CXX=${TRILINOS_ROOT}/bin/nvcc_wrapper
+module load openmpi cmake openblas likwid
+
+export TRILINOS_ROOT=/beegfs/apps/unsupported/trilinos-devel/compute/
+export OMPI_CXX=g++
 export CMAKE_PREFIX_PATH=${TRILINOS_ROOT}/lib/cmake:${CMAKE_PREFIX_PATH}
+export OMP_PROC_BIND=close
+export OMP_PLACES=threads
