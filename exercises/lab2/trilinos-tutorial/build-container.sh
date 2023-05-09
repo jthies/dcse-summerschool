c087ecf31aac0692798426c5bd541d16992ecfeb
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-05-09 11:58:18 +0200

    remove docker-related files (will work with central installation on DelftBlue)

--- exercises/lab2/trilinos-tutorial/build-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t trilinos_demo -f dockerfiles/trilinos_demo configure-files
--- exercises/lab2/trilinos-tutorial/run-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -i -v `pwd`:/opt/trilinos_demo -t trilinos_demo /bin/bash
