192362716841e86b4e856f0b0ea07045980b869f
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-05-27 21:59:19 +0200

    update env-script for Trilinos to hopefully work on CPU and GPU

--- lab2/trilinos-env.sh
+++ lab2/trilinos-env.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-module use /apps/generic/compiler/lmod/linux-rhel8-x86_64/Core
-module load gcc/11.3.0
-module use /apps/arch/2023rc1/lmod/linux-rhel8-x86_64/gcc/11.3.0
+if [[ "${HOSTNAME}" =~ "gpu" ]]; then
+  module load 2022r2
+  module load cuda/11.1.1
+  module load intel/oneapi-all
+  export TRILINOS_ROOT=/beegfs/apps/unsupported/trilinos-devel/gpu/
+  # Trilinos uses a special wrapper to allow compiling Kokkos code either for CUP or GPU,
+  # this makes sure that this wapper is used by the MPI compiler wrapper.
+  export OMPI_CXX=${TRILINOS_ROOT}/bin/nvcc_wrapper
+else
+  module use /apps/generic/compiler/lmod/linux-rhel8-x86_64/Core
+  module load gcc/11.3.0
+  module use /apps/arch/2023rc1/lmod/linux-rhel8-x86_64/gcc/11.3.0
+  module load openblas
+  export TRILINOS_ROOT=/beegfs/apps/unsupported/trilinos-devel/compute-likwid/
+  export OMPI_CXX=g++
+fi
 
-module load openmpi cmake openblas
+module load openmpi cmake likwid
 
-export TRILINOS_ROOT=/beegfs/apps/unsupported/trilinos-devel/compute/
-export OMPI_CXX=g++
 export CMAKE_PREFIX_PATH=${TRILINOS_ROOT}/lib/cmake:${CMAKE_PREFIX_PATH}
 export OMP_PROC_BIND=close
 export OMP_PLACES=threads
