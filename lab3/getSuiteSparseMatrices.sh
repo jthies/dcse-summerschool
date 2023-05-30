a535566d4ade5bd2ef499d7ab73dc9793db44794
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-05-30 08:42:35 +0200

    A script to download and unpack necessary matrices

---
+++ lab3/getSuiteSparseMatrices.sh
@@ -0,0 +1,28 @@
+cd $SCRATCH
+mkdir -p suite-sparse-matrices
+cd suite-sparse-matrices
+
+matrices="af_shell10 kkt_power"
+webLinks="https://suitesparse-collection-website.herokuapp.com/MM/Schenk_AFE/af_shell10.tar.gz https://suitesparse-collection-website.herokuapp.com/MM/Zaoui/kkt_power.tar.gz"
+
+ctr=1
+for matrix in $matrices; do
+	webLink=$(echo ${webLinks} | cut -d" " -f${ctr})
+	if [ ! -f "${matrix}.mtx" ]; then
+		echo "Downloading ${matrix} matrix from SuiteSparse collection"
+		wget ${webLink}
+
+		echo "Unpacking ${matrix} matrix"
+		tar -xvzf ${matrix}.tar.gz
+		mv ${matrix}/${matrix}.mtx .
+
+		#remove tars and folders
+		rm -rf ${matrix} ${matrix}.tar.gz
+	else
+		echo "Found ${matrix} matrix"
+	fi
+	let ctr=${ctr}+1
+done
+
+cd -
+echo "Done"
