e8f67ae7efa69c5cb96e56f2c7eeb1316066db3e
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-06-03 23:05:23 +0200

    fix gemm in solution (dimensions are quite tricky!)

--- gpu-lab/solution/matmatprod-cublas.cu
+++ gpu-lab/solution/matmatprod-cublas.cu
@@ -1,4 +1,5 @@
-/* Matrix Vector Multiplication example using CuBLAS
+/* Matrix Vector Multiplication example using CuBLAS_T
+
  * 
  * Performs the operation : b = A * x
  * 
@@ -21,17 +22,20 @@
 
 /* multiply C = A*B, where A, B and C are stored in row-major order,
    A is (n x n), B and C are n x k (n rows and k columns).
-   Note that due to the fact that BLAS routines assume column-major ordering,
-   we need to pretend we do the operation C = (A^T*B^T), where OP(B)=B^T is n x k.
-*/
+ */
 void matmatProdGPU(float const* d_A, float const* d_B, float *d_C, long n, long k, long threadsPerBlock)
 {
 
    float alpha=1.0, beta=0.0;
-   /* note: because we use row-major storage of A, we have to ask for the transposed operation
-      from CuBLAS, which also means we need to swap the row and column dimensions...
+
+   /* note: because we use row-major storage of A, B and C, we have to ask for the transposed operation
+      from CuBLAS, which also means we need to swap the row and column dimensions.
+      C = A*B = (B^T*A^T)^T
+
+      According to the documentation, we need to pass in AA, BB and CC such that CC = opA(AA)*opB(BB), and N, M, K s.t. opA(AA) is MxK, op(BB) is KxN and CC is MxN
+      For us this means: AA^T=B^T is k x n, BB^T=A^T is n x n, and CC=C^T is k x n
     */
-   cublasSgemm(CUBLAS_OP_T, CUBLAS_OP_T, n, n, k, alpha, d_A, n, d_B, k, beta, d_C, k);
+   cublasSgemm('T', 'T', n, k, n, alpha, d_B, n, d_A, n, beta, d_C, k);
    cudaGetLastError();
    checkCudaError("Kernel execution error !");
 }
